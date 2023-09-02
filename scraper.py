20c1c4c2a1cdc863c3274aceb2ececbbb9d2b9cf
levtus <61996958+alexandsr@users.noreply.github.com>
2023-09-02 16:19:54 -0500

    add emails

--- scraper.py
+++ scraper.py
@@ -1,23 +1,36 @@
 import requests
+import re
 from bs4 import BeautifulSoup
+import fileinput
+import sys
+email_pattern = r'.+\@.+\..+'
 
 # Get all the subpage links
-url = 'https://www.svenska-ambassaden.com/2.php?c=Sverige'
-grab = requests.get(url)
-soup = BeautifulSoup(grab.text, 'html.parser')
+#url = 'https://www.svenska-ambassaden.com/2.php?c=Sverige'
+#grab = requests.get(url)
+#soup = BeautifulSoup(grab.text, 'html.parser')
  
-f = open("linklist.txt", "w")
-for link in soup.find_all("a"):
-   data = link.get('href')
-   f.write(data)
-   f.write("\n")
- 
-f.close()
+#f = open("linklist.txt", "w")
+#print('Writing links to embassy pages to file.')
+#for link in soup.find_all("a"):
+#   data = link.get('href')
+#   f.write(data + "\n")
+#print('Finished writing links to embassy pages.')
+#f.close()
+
+
+#for line in fileinput.input(['./linklist.txt'], inplace=True):
+#    sys.stdout.write('https://www.svenska-ambassaden.com/{l}'.format(l=line))
+
 g = open("linklist.txt", "r")
 urls = g.readlines()
 for i in urls:
-    take = requests.get(urls[i])
-    suppe = BeautifulSoup(take.text, 'html.parser')
-    data = link.get('href')
-    match = requests.search(r'[\w.+-]+@[\w-]+\.[\w.-]+', link)
-    out = open("emails.txt", "w")
+   print(i)
+   take = requests.get(i)
+   soup = BeautifulSoup(take.text, 'html.parser')
+
+   # Find all email addresses in the take variable using the regular expression pattern
+   mydivs = soup.findAll('div')
+   for div in mydivs: 
+      with open('divs.txt', 'w') as d:
+         d.write(div + '\n')
